5fbfa841ff239b8624659b9817c55c2f1200ec60
Franz Auernigg <f.auernigg@commend.com>
2022-03-24 15:43:24 +0100

    parse options.json and create ffserver.conf

--- videoserver/rootfs/etc/cont-init.d/videoserver.sh
+++ videoserver/rootfs/etc/cont-init.d/videoserver.sh
@@ -2,7 +2,7 @@
 # ==============================================================================
 # Prepare the ffserver service for running
 # ==============================================================================
-declare interface
+declare port
 export HOSTNAME
 
 # Read hostname from API or setting default "hassio"
@@ -13,17 +13,69 @@ if bashio::var.is_empty "${HOSTNAME}"; then
     HOSTNAME="hassio"
 fi
 
-# Get default interface
-#interface=$(bashio::network.name)
-
-#bashio::log.info "Using hostname=${HOSTNAME} interface=${interface}"
-interface=enp0s31f6
-
-cp /usr/share/tempio/ffserver.gtpl /etc/ffserver.conf
+#cp /usr/share/tempio/ffserver.gtpl /etc/ffserver.conf
 
 # Generate configuration.
-jq ".interface = \"${interface}\"" /data/options.json \
+jq ".port = \"${port}\"" /data/options.json \
     | tempio \
       -template /usr/share/tempio/ffserver.gtpl \
       -out /etc/ffserver.conf
+
+
+
+function startStream()
+{
+	while [ 1 ] ; do
+		sleep 1s
+		echo "start ffmpeg $1 from input: $2"
+		#pid=$(ps aux | grep -v grep | grep ffmpeg | grep "$1" | grep "$2")
+		#if [ -n "$pid"  ] ; then
+		#	kill -9 $(echo $pid | cut -d ' ' -f1)
+		#fi
+		ffmpeg -i "$2" -strict -2 "http://localhost:8090/$1.ffm"
+		echo "ffmpeg exit done errorcode: $?"
+	done
+}
+
+function addServerConf()
+{
+    echo -e "\n"\
+    "<Feed $1.ffm>\n"\
+    "    File    /run/$1.ffm\n"\
+    "    FileMaxSize 10M\n"\
+    "</Feed>\n\n"\
+    "<Stream $1.mjpeg>\n"\
+    "   Feed $1.ffm\n"\
+    "   Format $2\n"\
+    "   VideoFrameRate $3\n"\
+    "   VideoIntraOnly\n"\
+    "   VideoBufferSize 4096\n"\
+    "   VideoSize $4\n"\
+    "   VideoQMin 5\n"\
+    "   VideoQMax 51\n"\
+    "   NoAudio\n"\
+    "   Strict -1\n"\
+    "</Stream>\n"\
+    >> ffserver.conf
+}
+
+function parseFFMpegConf()
+{
+	json=options.json
+	for row in $(jq -cr '.video_sources[]' $json); do
+		name=$(echo $row | jq -r '.name')
+		input=$(echo $row | jq -r '.input')
+        fmt=$(echo $row | jq '.format')
+
+		if [[ -n "$name" && -n "$input" ]] ; then
+            addServerConf "$name" "$fmt" 3 "640x360"
+			startStream "$name" "$input" &
+		else
+			echo "Error invalid config, misssing name or input: $row"
+		fi
+	done
+}
+
+parseFFMpegConf
+
 exit 0
--- videoserver/rootfs/usr/share/tempio/ffserver.gtpl
+++ videoserver/rootfs/usr/share/tempio/ffserver.gtpl
@@ -1,30 +1,6 @@
-# example to use ffmpeg, ffserver
-#   netbios name = {{ env "HOSTNAME" }}
-#   interfaces = {{ .interface }}
-
-HTTPPort            8090
+HTTPPort            {{ .port }}
 HTTPBindAddress     0.0.0.0
-#HTTPBindAddress     {{ .interface }}
 MaxHTTPConnections 200
 MaxClients      100
 MaxBandWidth    500000
 CustomLog       /run/ffserver.log
-
-<Feed n.ffm>
-File            /run/n.ffm
-FileMaxSize     10M
-</Feed>
-
-
-<Stream n.mjpeg>
-Feed n.ffm
-Format mpjpeg
-VideoFrameRate 3
-VideoIntraOnly
-VideoBufferSize 4096
-VideoSize 640x360
-VideoQMin 5
-VideoQMax 51
-NoAudio
-Strict -1
-</Stream>
