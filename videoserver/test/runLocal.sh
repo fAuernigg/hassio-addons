47c821252ff16562dd45959670e58366b46e303e
Franz Auernigg <f.auernigg@commend.com>
2022-04-19 00:44:56 +0200

    knxdevice2json & videoserver: cleanup repo

--- videoserver/test/runLocal.sh
+++ videoserver/test/runLocal.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 mkdir -p /tmp/my_test_data
 cp options.json /tmp/my_test_data
-docker build --build-arg BUILD_FROM="homeassistant/armv7-base:latest" -t fAuernigg/armv7-addon-videoserver ..
-docker run --rm -v /tmp/my_test_data:/data -p 5300 dianlight/armv7-addon-videoserver
+docker build --build-arg BUILD_FROM="homeassistant/armv7-base:latest" -t fauernigg/armv7-addon-videoserver ..
+docker run --rm -v /tmp/my_test_data:/data -p 5300 fauernigg/armv7-addon-videoserver
