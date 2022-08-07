9b347e9cde18eefbfcc15096fac69fd386570408
Franz Auernigg <f.auernigg@commend.com>
2022-08-07 15:44:32 +0200

    knxdevice2json: preconfigure mqtt client

---
+++ knxdevice2json/rootfs/etc/cont-init.d/mosquitto.sh
@@ -0,0 +1,21 @@
+#!/command/with-contenv bashio
+declare host
+declare password
+declare port
+declare username
+
+if bashio::services.available "mqtt"; then
+  host=$(bashio::services "mqtt" "host")
+  password=$(bashio::services "mqtt" "password")
+  port=$(bashio::services "mqtt" "port")
+  username=$(bashio::services "mqtt" "username")
+  mkdir -p /root/.config
+  {
+    echo "-h ${host}"
+    echo "--pw ${password}"
+    echo "--port ${port}"
+    echo "--username ${username}"
+  } > /root/.config/mosquitto_sub
+  ln -s /root/.config/mosquitto_sub /root/.config/mosquitto_pub
+  ln -s /root/.config/mosquitto_sub /root/.config/mosquitto_rr
+fi
